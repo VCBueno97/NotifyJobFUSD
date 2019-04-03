3132e55d20b30600b257aaba463dea6d9634fd63
Victor <vcbueno97@gmail.com>
2019-04-03 03:11:07 -0700

    Discord Bot now able to send message to chat

--- NotifyJobFUSD/Program.cs
+++ NotifyJobFUSD/Program.cs
@@ -17,9 +17,25 @@ namespace Notify
 
         static void Main(string[] args)
             => new Program().StartAsync().GetAwaiter().GetResult();
-   
+         
 
-        public static async Task ReadPage()
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+        public async Task StartAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+            await _client.LoginAsync(TokenType.Bot, "NTYyNzQ5MzgxNzUzODMxNDQ4.XKPw3A.lcNR_3DOBcJMvr402ahimeiw8jE");
+            await _client.StartAsync();
+            ReadPage();
+            await Task.Delay(-1);
+
+        }
+        public async Task ReadPage()
         {
             var url = "https://jobs.fresnounified.org/ats/job_board?COMPANY_ID=00001115";
             var httpClient = new HttpClient();
@@ -29,7 +45,6 @@ namespace Notify
             bool JobFound = false;
 
             htmlDoc.LoadHtml(html);
-
             var evenDivs = htmlDoc.DocumentNode.Descendants("tr")
                 .Where(node => node.GetAttributeValue("class", "")
                 .Equals("even")).ToList();
@@ -54,21 +69,14 @@ namespace Notify
                     JobFound = true;
                 }
             }
+            if(JobFound == true) 
+            {
+              await _client.GetGuild(562750653416472592).GetTextChannel(562750653454221364).SendMessageAsync("JOB FOUND");
+            }
+            else 
+            {
+              await _client.GetGuild(562750653416472592).GetTextChannel(562750653454221364).SendMessageAsync("No Software Developer Job Found Yet");
+            }
         }
-        private Task Log(LogMessage msg)
-        {
-            Console.WriteLine(msg.ToString());
-            return Task.CompletedTask;
-        }
-        public async Task StartAsync()
-        {
-            _client = new DiscordSocketClient();
-            _client.Log += Log;
-            await _client.LoginAsync(TokenType.Bot, "NTYyNzQ5MzgxNzUzODMxNDQ4.XKPw3A.lcNR_3DOBcJMvr402ahimeiw8jE");
-            await _client.StartAsync();
-            await Task.Delay(-1);
-
-        }
-           
     }
 }
